a374b0acab3c73f2f8462dacd9e7ec8fbf69f011
井艸汰介 <taisuke.igusa@gmail.com>
2025-02-14 23:43:19 +0900

    たいブロ修正
    ・ゲームオーバー画面のボタン位置
    ・タイトル画面へ戻るボタン追加

--- Assets/Scripts/TaisBlockButtonScript.cs
+++ Assets/Scripts/TaisBlockButtonScript.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TaisBlockByttonScript : MonoBehaviour
+public class TaisBlockButtonScript : MonoBehaviour
 {
     public void StartGame()
     {
