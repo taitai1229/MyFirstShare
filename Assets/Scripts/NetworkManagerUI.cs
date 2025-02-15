11588ce5fc3d656cf7eafaafef08d252f4e167ad
井艸汰介 <taisuke.igusa@gmail.com>
2025-02-15 15:58:50 +0900

    マルチプレイゲーム
    途中まで

---
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button clientButton;
+    private void Awake()
+    {
+        hostButton.onClick.AddListener(() => 
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+        clientButton.onClick.AddListener(() => 
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
