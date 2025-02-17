84ecf0fd5d9b0fd34a3ed9c7f30196bd658077e2
miyama09 <potatorice.g@gmail.com>
2025-02-18 04:14:34 +0900

    gameover追加
    いぐさが作ったやつをこぴーしただけ

--- Assets/miyamaScripts/BallController.cs
+++ Assets/miyamaScripts/BallController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; // シーン管理のために追加
 
 public class BallController : MonoBehaviour
 {
@@ -11,6 +12,15 @@ public class BallController : MonoBehaviour
 
         // ボールの初期方向をランダムに設定
         Vector2 initialDirection = new Vector2(Random.Range(-1f, 1f), 1).normalized;
-        rb.linearVelocity = initialDirection * speed;
+        rb.linearVelocity = initialDirection * speed; // 修正: linearVelocity → velocity
+    }
+
+    void Update()
+    {
+        // ボールが y = -10 より下に落ちたらゲームオーバーシーンへ
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
     }
 }
